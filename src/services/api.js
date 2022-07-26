f6d1ddbb3c2b81921443b0ae95269200cea80068
Agnes <agnes.rosa@hotmail.com>
2022-07-26 20:44:24 -0300

    fix: api baseurl added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://jobinhos-api.herokuapp.com",
+  baseURL: "https://sparmapi.herokuapp.com/",
 });
 
 export default api;
